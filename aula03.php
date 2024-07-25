d2580a397ecafc93fb3ac3ae30341d0e27d11b1e
alanaleticia04 <169085788+alanaleticia04@users.noreply.github.com>
2024-07-25 14:58:26 -0300

    Add files via upload

---
+++ aula03.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>contando de 1 a 10.</h1>";
+
+
+    for($x=1; $x<=10; $x++){
+        echo "$x <br>";
+    };
+
+    echo "<h1>tabela de 1 a 10 em PHP</h1>";
+
+    
+
+
+
+    ?>
+</body>
+</html>
