c61f17a5dece6a6ec0d3c2bc1317f3a1619bc6b3
alanaleticia04 <169085788+alanaleticia04@users.noreply.github.com>
2024-08-23 16:34:15 -0300

    Add files via upload

--- aula04.php
+++ aula04.php
@@ -11,23 +11,29 @@
         td{
             border: 1px solid black;
         }
+        img{
+            width: 100px;
+        }
     </style>
 </head>
 <body>
     <?php
-    echo "<h1>contando de 1 a 10.</h1>";
+    echo "<h1>salada de fruta.</h1>";
 
 
+
+    echo "<table><tr>";
+    for($x=1; $x<=10; $x++){
+        echo "<td><img src=' img/foto $x.jpg'></td>";
+    }
+    echo "</tr></table>";
+
+    echo "<h1>tabela de 1 a 10 em PHP</h1>";
     for($x=1; $x<=10; $x++){
         echo "$x <br>";
     };
 
-    echo "<h1>tabela de 1 a 10 em PHP</h1>";
-    echo "<table><tr>";
-    for($x=1; $x<=10; $x++){
-        echo "<td>$x</td>";
-    }
-    echo "</tr></table>";
+
 
 
 
